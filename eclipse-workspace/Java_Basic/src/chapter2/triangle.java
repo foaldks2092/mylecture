a564ae30b0acad08e4c4e8b574f7e753904fb7ce
foaldks2092 <foaldks2092@naver.com>
2021-05-24 17:37:48 +0900

    피라미드

---
+++ eclipse-workspace/Java_Basic/src/chapter2/triangle.java
@@ -0,0 +1,25 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("입력하시오: ");
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = n; j > i-1; j--) {
+				System.out.print(" ");				
+			}
+			for (int k = 0; k < i*2+1 ; k++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
